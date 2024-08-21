7715733a280e84c22ccc32c15643580dbc86c4b5
Isaac Mora <Isaacmorap@outlook.com>
2024-08-21 18:25:27 -0400

    Changes to be committed: 	modified:   css/styles.css 	modified:   index.html 	modified:   js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,16 @@ $(document).ready(function () {
       alert("Por favor, completa todos los campos.");
     } else {
       alert("Mensaje enviado correctamente.");
-      // Aquí podrías agregar lógica para enviar el formulario vía AJAX o resetear el formulario.
+      /* TODO Aquí se agregaria la logica para enviar el form de contacto,
+      probablemente la agregue si sigo trabajando en este portafolio*/
       $(this)[0].reset();
     }
   });
 
 
-  // Efecto de scroll suave para los enlaces del menú
+  /* Efecto de scroll suave para los enlaces del menú
+  Esto simplemente hace que el movimiento sea mas suave
+  */
   $("a.nav-link").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
